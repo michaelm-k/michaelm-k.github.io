6f95ebc62f7553a81e3b188a190a11c3c0152079
Michael <atlikeross@gmail.com>
2014-12-29 13:15:52 -0500

    Revert "intro animations slightly delayed further"
    This reverts commit 1f19fd18e5ee8ffd9643d0f6aebb16ca0bc616fa.

--- js/script.js
+++ js/script.js
@@ -448,7 +448,7 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
 setTimeout(function(){
 $( "a.writer" ).css( "cursor", "pointer" );
 $('.writer').animate({opacity:1}, 2000); 
-},15500);
+},15300);
 
 $("a.writer").click(function() {
 	$("a.writer").addClass( 'active' );
@@ -484,7 +484,7 @@ $( "a.writer" )
 /* END: LEAVE PAGE */ 
 setTimeout(function(){
 $('.navbar-inverse').removeClass('slideIn');
-},15500);
+},15300);
 setTimeout(function(){
  $(".static-footer").slideToggle();
-},15950);
+},15750);
