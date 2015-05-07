45dffc4473f9a43b00e4ae60425efbc618f89ac4
Michael <atlikeross@gmail.com>
2015-05-06 23:51:21 -0400

    Blog officially linked + code update
    Check it out :0

---
+++ js/other.js
@@ -0,0 +1,23 @@
+$(document).snowfall();			
+
+function popup(){
+	playerWindow = window.open("http://michaelm-k.github.io/player", "player", "location=no,width=365,height=487");
+}	
+			
+// http://support.cargocollective.com/customer/portal/questions/1349490-animated-gif-for-header-img-force-gif-to-restart-on-refresh
+$(function(){
+	
+	// replace src with timestamped url to avoid cache
+	$('#name').hide().attr('src', $('#name').attr('src') + '?_=' + Date.now());
+				
+	// hide the image until the new one has loaded. Otherwise it'll look choppy when the new one loads.
+	$('#name').one('load', function() {
+		$(this).show();
+	}).each(function() {	
+		// also fire the function if the image is loaded before the event is bound.
+		if (this.complete) {
+			$(this).load();
+		}
+	});
+	
+});		
