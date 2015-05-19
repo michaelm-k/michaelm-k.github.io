8b24778f1c399303ba5942794209537637d90610
Michael <atlikeross@gmail.com>
2015-05-18 23:44:59 -0400

    Didn't work

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ if ($( "#content" ).has( "#name" )){
 /* START: SOCIAL ICONS */ 
 var clicked=0;
 var hovering=0;
+
 $( ".navbar-inverse .navbar-brand" ).hover(
   function() {
     $(this).animate( {color: '#bdbcae '}, "200" );
@@ -28,9 +29,11 @@ $( ".navbar-inverse .navbar-brand" ).hover(
 	hovering=0;
   }
 );
+
 $( ".navbar-inverse .navbar-brand" ).mousedown(function() {
 		$(this).animate( {color: '#002e63 '}, "50" ); 
 });
+
 $(".navbar-inverse .navbar-brand").click(function() {   
 	clicked=1;
 	if (hovering==1){
@@ -57,16 +60,9 @@ $(".static-footer .fa-soundcloud, .static-footer .fa-stack-overflow, .static-foo
 	$(this).stop(true).animate( {color: '#ffffff'}, "slow" );     
 });
 /* END: SOCIAL ICONS */ 
-
-  
+ 
 var counter = 0;
 var scrolling=false;
-var scrolls1=0;
-var scrolls2=0;
-var hovering=0;
-var scrollingTop=0;
-
-
 const EMPTY_COUNTER = 0, FULL_COUNTER = 2;
 
 var shouldOpen = function () {
@@ -94,9 +90,6 @@ var scrollTop = function () {
     });
     var refresh = function () { 
         scrolling=false;
-		scrollingTop=0;
-		scrolls1=0;
-		scrolls2=0;
     };
 })();
 
@@ -183,7 +176,8 @@ function loadAbout() {
 		});
 	},2000);
 }
-function loadWork() {
+
+function loadProjects() {
 	$( "#storyimg" )
 	.mouseover(function() { 
 		$(this).animate( {opacity: '1'}, 1000 ); 		
@@ -192,118 +186,95 @@ function loadWork() {
 		$(this).stop(true).animate( {opacity: '0.5'}, 1000 ); 			
 	});
 }
+
 function loadContact() {
-	$("#dave").stop(true).animate( {opacity: '1'}, 2000); 
-	
+	$("#dave").stop(true).animate( {opacity: '1'}, 2000); 	
 	$('#content-text .fa-diamond, #content-text .fa-stack-overflow, #content-text .fa-linkedin-square, #content-text .fa-github-square').addClass('animated rubberBand');
 }
 
-/* START: ABOUT & CONTACT LINKS */
+function prepContent() {
+	event.preventDefault();
+	$( "#content" ).css( "position", "relative" ); 
+	$(".custom-wrapper").css("overflow", "hidden");
+}
+
+function formatContent() {
+	$(".custom-wrapper").css("overflow", "hidden");		
+	var hiddenDiv = document.getElementById('content');
+	var docWidth = window.innerWidth;
+	hiddenDiv.style.position = 'relative';
+	hiddenDiv.style.display = 'inline-block';
+}
+
+/* START: NAV CONTENT TRANSITIONS */
 $(".navbar-inverse .navbar-nav > li > a").click(function() { 
 	$("html, body").css("overflow", "visible");
 	if (!$(event.target).closest("#tab2").length && !$(event.target).closest("#tab5").length) {	
-		if ($(event.target).closest("#tab1").length && !$(this).closest('li').hasClass( "active" )) {
-			if ($('li.active a').attr('id') == "tab4" || $('li.active a').attr('id') == "tab3") {
-				event.preventDefault();
-				$( "#content" ).css( "position", "relative" ); 
-				$(".custom-wrapper").css("overflow", "hidden");
+		if ($(event.target).closest("#tab1").length && !$(this).closest('li').hasClass( "active" )) { // clicked on ABOUT
+			if ($('li.active a').attr('id') == "tab4" || $('li.active a').attr('id') == "tab3") { // at CONTACT or PROJECTS
+				prepContent();
 				$("#content").stop(true).animate({left:'+110%'}, 1000, function() {
-					$('#content').load('about.html #content', function() {//ABOUT.HTML
-						window.history.replaceState("", "", '/about');
-						var hiddenDiv = document.getElementById('content');
-						var docWidth = window.innerWidth;
-						hiddenDiv.style.position = 'relative';
-						hiddenDiv.style.display = 'inline-block';
-						hiddenDiv.style.left = -docWidth + "px";
-						$(".custom-wrapper").css("overflow", "hidden");
-						$("#content").animate({left:'0px'}, 1000, function(){ //.stop(true) isn't present here because it made it possible to glitch shit
-														
-							if($(window).scrollTop() !== 0 && scrolling==false) {	
-								scrollTop();
-							}
-							//$("#photocap").css("display", "block");
-							
+					$('#content').load('about.html #content', function() {
+						window.history.replaceState("", "", '/about');  // comment out during development
+						formatContent();
+						document.getElementById('content').style.left = -window.innerWidth + "px"; // enter content from left		
+						$("#content").animate({left:'0px'}, 1000, function(){ //.stop(true) isn't present here because it made it possible to glitch shit					
 							loadAbout();
-							
+							if ($(window).scrollTop() !== 0 && scrolling == false) {	
+								scrollTop();
+							}
+						});
+					});
+				});
+				$('li').removeClass( 'active' );
+				$(this).closest('li').addClass( 'active' );		
+			} else { // at home
+				prepContent();
+				$("#content").stop(true).animate({left:'-110%'}, 1000, function() {
+					$('#content').load('about.html #content', function() {
+						window.history.replaceState("", "", '/about'); // comment out during development
+						formatContent();
+						document.getElementById('content').style.left = window.innerWidth + "px"; // enter content from right
+						$("#content").animate({left:'0px'}, 1000, function() {						
+							loadAbout();	
+							if($(window).scrollTop() !== 0 && scrolling == false) {	
+								scrollTop();
+							}				
 						});
 					});
 				});
 				$('li').removeClass( 'active' );
 				$(this).closest('li').addClass( 'active' );
-				
-			} else {
-				event.preventDefault();
-				$( "#content" ).css( "position", "relative" ); 
-				$(".custom-wrapper").css("overflow", "hidden");
-				$("#content").stop(true).animate({left:'-110%'}, 1000, function() {
-					$('#content').load('about.html #content', function() {//ABOUT.HTML
-						window.history.replaceState("", "", '/about');
-						var hiddenDiv = document.getElementById('content');
-						var docWidth = window.innerWidth;
-						hiddenDiv.style.position = 'relative';
-						hiddenDiv.style.display = 'inline-block';
-						hiddenDiv.style.left = docWidth+'px';
-						$(".custom-wrapper").css("overflow", "hidden");
-						$("#content").animate({left:'0px'}, 1000, function() {
-													
-							if($(window).scrollTop() !== 0 && scrolling==false) {	
-								scrollTop();
-							}
-							//$("#photocap").css("display", "block");	
-							loadAbout();					
-						});
-					});
+			}				
+		} else if ($(event.target).closest("#tab4").length && !$(this).closest('li').hasClass( "active" )) { // clicked on CONTACT
+			prepContent();
+			$("#content").stop(true).animate({left:'-110%'}, 1000, function() {
+				$('#content').load('contact.html #content', function() {
+					window.history.replaceState("", "", '/contact'); // comment out during development
+					formatContent();
+					document.getElementById('content').style.display = 'block';
+					document.getElementById('content').style.left = window.innerWidth + "px"; // enter content from right		
+					$("#content").animate({left:'0px'}, 1000, function() {	
+						loadContact();
+						if ($(window).scrollTop() !== 0 && scrolling == false) {	
+							scrollTop();
+						}	
+					});					
 				});
-				$('li').removeClass( 'active' );
-				$(this).closest('li').addClass( 'active' );
-			}
-					
-		} else if ($(event.target).closest("#tab4").length && !$(this).closest('li').hasClass( "active" )){
-			event.preventDefault();
-			$( "#content" ).css( "position", "relative" ); 
-			$(".custom-wrapper").css("overflow", "hidden");
-				$("#content").stop(true).animate({left:'-110%'}, 1000, function() {
-					$('#content').load('contact.html #content', function() {//CONTACT.HTML 	
-						window.history.replaceState("", "", '/contact');
-						var hiddenDiv = document.getElementById('content');
-						var docWidth = window.innerWidth;
-						hiddenDiv.style.position = 'relative';
-						hiddenDiv.style.display = 'block';
-						hiddenDiv.style.left = docWidth+'px';
-						$(".custom-wrapper").css("overflow", "hidden");
-						$("#content").animate({left:'0px'}, 1000, function(){
-							
-							
-							loadContact();
-							if($(window).scrollTop() !== 0 && scrolling==false) {	
-								scrollTop();
-							}
-							
-						});					
-					});
-				});
-				$('li').removeClass( 'active' );
-				$(this).closest('li').addClass( 'active' );
-		} else if ($(event.target).closest("#tab3").length && !$(this).closest('li').hasClass( "active" )) {
-			if ($('li.active a').attr('id') == "tab4") {
-				event.preventDefault();
-				$( "#content" ).css( "position", "relative" ); 
-				$(".custom-wrapper").css("overflow", "hidden");
+			});
+			$('li').removeClass( 'active' );
+			$(this).closest('li').addClass( 'active' );
+		} else if ($(event.target).closest("#tab3").length && !$(this).closest('li').hasClass( "active" )) { // clicked on PROJECTS
+			if ($('li.active a').attr('id') == "tab4") { // at CONTACT
+				prepContent();
 				$("#content").stop(true).animate({left:'+110%'}, 1000, function() {
-					$('#content').load('projects.html #content', function() {//WORK.HTML
-						window.history.replaceState("", "", '/projects');
-						var hiddenDiv = document.getElementById('content');
-						var docWidth = window.innerWidth;
-						hiddenDiv.style.position = 'relative';
-						hiddenDiv.style.display = 'inline-block';
-						hiddenDiv.style.left = -docWidth + "px";
-						$(".custom-wrapper").css("overflow", "hidden");
-						
-						loadWork();
-						
+					$('#content').load('projects.html #content', function() {
+						window.history.replaceState("", "", '/projects'); // comment out during development
+						formatContent();
+						document.getElementById('content').style.left = -window.innerWidth + "px"; // enter content from left		
 						$("#content").animate({left:'0px'}, 1000, function() {
-							
-							if($(window).scrollTop() !== 0 && scrolling==false) {	
+							loadProjects();	
+							if($(window).scrollTop() !== 0 && scrolling == false) {	
 								scrollTop();
 							}	
 						});
@@ -311,24 +282,16 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
 				});
 				$('li').removeClass( 'active' );
 				$(this).closest('li').addClass( 'active' );
-			} else {
-				event.preventDefault();
-				$( "#content" ).css( "position", "relative" ); 
-				$(".custom-wrapper").css("overflow", "hidden");
+			} else { // at ABOUT or home
+				prepContent();
 				$("#content").stop(true).animate({left:'-110%'}, 1000, function() {
-					$('#content').load('projects.html #content', function() {//WORK.HTML
-						window.history.replaceState("", "", '/projects');
-						var hiddenDiv = document.getElementById('content');
-						var docWidth = window.innerWidth;
-						hiddenDiv.style.position = 'relative';
-						hiddenDiv.style.display = 'inline-block';
-						hiddenDiv.style.left = docWidth + "px";
-						$(".custom-wrapper").css("overflow", "hidden");
-						
-						loadWork();
-
+					$('#content').load('projects.html #content', function() {
+						window.history.replaceState("", "", '/projects'); // comment out during development
+						formatContent();
+						document.getElementById('content').style.left = window.innerWidth + "px"; // enter content from right		
 						$("#content").animate({left:'0px'}, 1000, function() {
-							if($(window).scrollTop() !== 0 && scrolling==false) {	
+							loadProjects();
+							if ($(window).scrollTop() !== 0 && scrolling == false) {	
 								scrollTop();
 							}		
 						});
@@ -340,14 +303,14 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
 		}
 	}	
 });
-/* END: ABOUT & CONTACT LINKS */
+/* END: NAV CONTENT TRANSITIONS */
 	
 /* START: HOME */
 	/* START: SKIP_INTRO */
 	var skipped=false;
 	var tooLatetoSkip=false;
-	$( ".skip_intro" ).css( "cursor", "pointer" );
-	$('.skip_intro').animate({opacity:1}, 2000); 
+	$(".skip_intro").css( "cursor", "pointer" );
+	$(".skip_intro").animate({opacity:1}, 2000); 
 
 	$(".skip_intro").click(function() {
 		$(".skip_intro").addClass( 'active' );
@@ -365,15 +328,17 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
 			skipped=true;
 			$('#content').load('about.html #content', function() {
 				window.history.replaceState("", "", '/about');		
-				$("#content").css("display", "none");
-				$( "#content" ).fadeIn( "slow" );
-				$('.navbar-inverse').removeClass('slideIn');		
-				$("html, body").css("overflow", "");
-				loadAbout();
-				$("#tab1").parent().addClass('active');
+				$('.navbar-inverse').removeClass('slideIn');	
 				setTimeout(function(){
 					$(".static-footer").slideToggle();
 				},500);	
+				$("#tab1").parent().addClass('active');
+				loadAbout();
+				
+				$("html, body").css("overflow", "visible");
+				$(window).on('beforeunload', function() {
+					$(window).scrollTop(0);
+				});				
 			});
 		}	
 	});
@@ -383,7 +348,7 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
 		$('.skip_intro').animate({opacity:0}, 2000);
 		$( ".skip_intro" ).css( "cursor", "default" );	
 	},15000);
-	/* END:  SKIP_INTRO */
+	/* END: SKIP_INTRO */
 
 	/* START: QUOTE */
 	setTimeout(function(){
@@ -408,46 +373,40 @@ $(".navbar-inverse .navbar-nav > li > a").click(function() {
  /* END: HOME */
  
 /* START: LEAVE PAGE */  
-  $(window).on("blur focus", function(e) {
-    var prevType = $(this).data("prevType");
-
-    if (prevType != e.type) {   //  reduce double fire issues
-        switch (e.type) {
-            case "blur":
-                $("a.writer").removeClass( "active" );
+$(window).on("blur focus", function(e) {
+	var prevType = $(this).data("prevType");
+	if (prevType != e.type) {   // reduce double fire issues
+		switch (e.type) {
+			case "blur":
+				$("a.writer").removeClass( "active" );
 				hovering=0;
-                break;
-            case "focus":
-                 $("a.writer").removeClass( "active" );
-				 hovering=0;
-                break;
-        }
-    }
-    $(this).data("prevType", e.type);
+				break;
+			case "focus":
+				$("a.writer").removeClass( "active" );
+				hovering=0;
+				break;
+		}
+	}
+	$(this).data("prevType", e.type);
 });
 /* END: LEAVE PAGE */ 
 
 if ($('li.active a').attr('id') == "tab4" || $('li.active a').attr('id') == "tab3" || $('li.active a').attr('id') == "tab1") {
-	$("#content").css("display", "none");
-	$( "#content" ).fadeIn( "slow" );
-	$('.navbar-inverse').removeClass('slideIn');		
-	$("html, body").css("overflow", "");
-		
-	//START: #tab1-specific
-		loadAbout();
-	//END: #tab1specific
-	
-	//START: #tab3-specific
-		loadWork();
-	//END: #tab3-specific
-	
-	//START: #tab4-specific
-		loadContact();
-	//END: #tab4-specific
-
+	$('.navbar-inverse').removeClass('slideIn');
 	setTimeout(function(){
 		$(".static-footer").slideToggle();
 	},500);
+	$("html, body").css("overflow", "visible");
+	$(window).on('beforeunload', function() {
+		$(window).scrollTop(0);
+	});		
+	if ($('li.active a').attr('id') == "tab1") {
+		loadAbout();
+	} else if ($('li.active a').attr('id') == "tab3") {
+		loadProjects();
+	} else {
+		loadContact();
+	}
 } else {
 	setTimeout(function(){
 		if (skipped==false) {
